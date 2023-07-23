7b0605266f953c995d59fb6b6b1ce160a095f5fe
otonielemanuel <otonielemanuel21@gmail.com>
2023-07-24 00:03:43 +0100

    add new section: galery

---
+++ assets/scripts/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.querySelector(".modal");
+    const modalImage = document.getElementById("modal-image");
+    const galleryItems = document.querySelectorAll(".gallery-item");
+    const closeBtn = document.querySelector(".close-btn");
+  
+    galleryItems.forEach((item) => {
+      item.addEventListener("click", function () {
+        modal.style.display = "flex";
+        modalImage.src = this.src;
+      });
+    });
+  
+    closeBtn.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+  
+    window.addEventListener("click", function (event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+  
