0cf51aef6caad41fbff62d66cdc0cc9ef2300893
otonielemanuel <otonielemanuel21@gmail.com>
2023-07-25 00:11:23 +0100

    session: whitness

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = document.querySelector(".modal");
-  const modalImage = document.getElementById("modal-image");
-  const galleryItems = document.querySelectorAll(".gallery-item");
-  const closeBtn = document.querySelector(".close-btn");
+const scrollLinks = document.querySelectorAll(".scroll-link");
 
-  galleryItems.forEach((item) => {
-    item.addEventListener("click", function () {
-      modal.style.display = "flex";
-      modalImage.src = this.src;
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
-  });
-
-  closeBtn.addEventListener("click", function () {
-    modal.style.display = "none";
-  });
-
-  window.addEventListener("click", function (event) {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
   });
 });
